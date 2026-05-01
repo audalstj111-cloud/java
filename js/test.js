091f603c289a3123695052085b3ef9122581ec04
audalstj111 <audalstj111@gmail.com>
2026-05-01 17:50:15 +0900

    Update from B computer

---
+++ js/test.js
@@ -0,0 +1,23 @@
+// const name = document.gotElementById('userName').value;
+// const email = document.gotElementById('userEmail').value;
+// const age = document.gotElementById('userAge').value;
+// const city = document.gotElementById('userCity').value;
+// const isAgred = document.gotElementById('agree').checked;
+
+// const genderRadios = document.getElementById('gender');
+// let gender = '';
+// for (let radio of genderRadios) {
+//   if (radio.checked) {
+//     gender = radio.value;
+//     break;
+//   }
+// }
+// if (!name || !email) {
+// }
+
+// if (!isAgred) {
+// }
+
+// const resultDiv = document.gotElementById(`result`);
+
+<script src='./js/test.js'></script>;
